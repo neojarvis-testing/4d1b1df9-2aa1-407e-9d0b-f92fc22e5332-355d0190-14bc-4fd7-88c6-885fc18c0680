c0dc8e93587f29e68b7172d54d81d3f22b5d4c67
sandeepkumarpradhan71 <sandeepkumarpradhan71@gmail.com>
2025-04-24 12:06:16 +0000

    Review Model

--- springapp/src/main/java/com/examly/springapp/dto/OrderDTO.java
+++ springapp/src/main/java/com/examly/springapp/dto/OrderDTO.java
@@ -1,5 +1,20 @@
 package com.examly.springapp.dto;
 
-public class OrderDTO {
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Data;
 
+
+@Data
+public class OrderDTO {
+    private Long id;
+    private LocalDate orderDate;
+    private String orderStatus;
+    private String shippingAddress;
+    private String billingAddress;
+    private Double totalAmount;
+    private Long userId;
+    private List<OrderItemDTO> orderItems;
 }
+
+
--- springapp/src/main/java/com/examly/springapp/model/Review.java
+++ springapp/src/main/java/com/examly/springapp/model/Review.java
@@ -1,5 +1,31 @@
 package com.examly.springapp.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
 public class Review {
-private long reviewId;
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+     private String reviewText;
+     private Integer rating;
+    private LocalDate date;
+
+     @ManyToOne
+     @JoinColumn(name = "userId")
+     private User user;
+
+     @ManyToOne
+     @JoinColumn(name = "productId")
+     private Product product;
 }
