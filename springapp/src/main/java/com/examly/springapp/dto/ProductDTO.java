4d706e93b211752f6ce64ca79d385d8bc5446f36
punitha203 <tpunithareddy@gmail.com>
2025-04-24 17:48:10 +0530

    Update ProductDTO.java

--- springapp/src/main/java/com/examly/springapp/dto/ProductDTO.java
+++ springapp/src/main/java/com/examly/springapp/dto/ProductDTO.java
@@ -1,7 +1,5 @@
 package com.examly.springapp.dto;
 
-public class ProductDTO {
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
