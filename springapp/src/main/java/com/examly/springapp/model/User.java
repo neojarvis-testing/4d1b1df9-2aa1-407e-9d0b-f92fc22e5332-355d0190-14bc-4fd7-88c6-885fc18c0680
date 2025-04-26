a02f7b75f4128fbe943207c721ec24c5dcdd90f0
vaidyaakanksha <vaidyaakanksha1@gmail.com>
2025-04-26 07:23:56 +0000

    user comments

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -5,7 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users") //Specify the table name
 public class User {
@@ -27,53 +33,4 @@ public class User {
     @Column(name = "user_role")
     private String userRole;
 
-    //Default constructor
-    public User() {
-    }
-
-    //Parameterised constructor
-    public User(long userId, String email, String password, String username, String mobileNumber, String userRole) {
-        this.userId = userId;
-        this.email = email;
-        this.password = password;
-        this.username = username;
-        this.mobileNumber = mobileNumber;
-        this.userRole = userRole;
-    }
-    public long getUserId() {
-        return userId;
-    }
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-    public String getUserRole() {
-        return userRole;
-    }
-    public void setUserRole(String userRole) {
-        this.userRole = userRole;
-    }
 }
