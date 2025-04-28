0291532e8b636e7363bdb65243e894a1e1b43ae5
vaidyaakanksha <vaidyaakanksha1@gmail.com>
2025-04-28 16:49:41 +0000

    login-register

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/models/user.model.ts
+++ angularapp/src/app/models/user.model.ts
@@ -9,13 +9,16 @@ export interface User {
  
     //Required:Must be provided while registering and logging in
     password: string; 
+
+    //Required:Must be provided while registering
+    confirmPassword:string;
  
     // Required: Unique identifier for the user
     username: string; 
  
-   // Required: Include proper format validation
-   mobileNumber: string;
+    // Required: Include proper format validation
+    mobileNumber: string;
  
     // Specify the user role "ADMIN" | "USER"
-    userRole: string;
+    userRole?: string;
 }
