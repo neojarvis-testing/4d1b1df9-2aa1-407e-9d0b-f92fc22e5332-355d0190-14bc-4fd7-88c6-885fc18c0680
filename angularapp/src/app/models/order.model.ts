1ed79c0da9d13c22197c9fb6e4ab05640f2994d3
sandeepkumarpradhan71 <sandeepkumarpradhan71@gmail.com>
2025-05-01 11:14:11 +0000

    review

--- angularapp/src/app/models/order.model.ts
+++ angularapp/src/app/models/order.model.ts
@@ -8,7 +8,7 @@ export interface Order{
     orderId?:number;
 
     // The date when the order was placed
-    orderDate:Date;
+    orderDate:string;
 
     // Current status of the order (e.g., Pending, Shipped, Delivered)
     orderStatus:string;
