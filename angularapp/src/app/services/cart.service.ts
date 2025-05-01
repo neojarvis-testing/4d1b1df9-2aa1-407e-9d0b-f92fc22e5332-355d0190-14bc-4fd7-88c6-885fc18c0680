a3842afff0ef0bb603d7220e9d5457923453b55b
sandeepkumarpradhan71 <sandeepkumarpradhan71@gmail.com>
2025-05-01 11:45:13 +0000

    order placed successfully

--- angularapp/src/app/services/cart.service.ts
+++ angularapp/src/app/services/cart.service.ts
@@ -8,32 +8,41 @@ import { Product } from '../models/product.model';
 })
 export class CartService {
 
-  private cartItems: OrderItem[] = [];
-  private cartSubject = new BehaviorSubject<OrderItem[]>(this.cartItems);
-  constructor(){ }
  
-  addToCart(product: Product, quantity: number): void {
-    const existingItem = this.cartItems.find(item => item.product.productId === product.productId);
-    if (existingItem) {
-      existingItem.quantity += quantity;
-    } else {
-      const price = product.price;
-      this.cartItems.push({ product, quantity, price });
-    }
-    this.cartSubject.next(this.cartItems);
-  }
+private cartItems: OrderItem[] = [];
+  private cartSubject = new BehaviorSubject<OrderItem[]>(this.cartItems);
 
- 
-  removeFromCart(productId: number): void {
-  this.cartItems = this.cartItems.filter(item => item.product.productId !== productId);
-  this.cartSubject.next(this.cartItems);
-  }
-  getCartItems(): OrderItem[] {
-    return [...this.cartItems];
-  }
-  clearCart(): void {
-  this.cartItems = [];
-  this.cartSubject.next(this.cartItems);
-  }
+  constructor() { }
+
+  addToCart(product: Product, quantity: number): void {
+    const existingItem = this.cartItems.find(item => item.productId === product.productId);
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      const orderItem: OrderItem = {
+        productId: product.productId,
+        productName: product.productName,
+        quantity: quantity,
+        price: product.price
+      };
+      this.cartItems.push(orderItem);
+    }
+    this.cartSubject.next(this.cartItems);
+  }
+
+  removeFromCart(productId: number): void {
+    this.cartItems = this.cartItems.filter(item => item.productId !== productId);
+    this.cartSubject.next(this.cartItems);
+  }
+
+  getCartItems(): OrderItem[] {
+    return [...this.cartItems];
+  }
+
+  clearCart(): void {
+    this.cartItems = [];
+    this.cartSubject.next(this.cartItems);
+  }
+
 
 }
