0ff0c07e0ba49061e36f191b4d90e27dead71c77
sandeepkumarpradhan71 <sandeepkumarpradhan71@gmail.com>
2025-05-02 04:42:12 +0000

    Quantity Update

--- angularapp/src/app/components/userviewproduct/userviewproduct.component.ts
+++ angularapp/src/app/components/userviewproduct/userviewproduct.component.ts
@@ -12,44 +12,53 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class UserviewproductComponent implements OnInit {
 
-  products:Product[]=[]
-  ApiUrl:string=Api.apiUrl
-  quantity:number
-  constructor(private router:Router,private productService:ProductService, private cartService:CartService) { }
-
-  ngOnInit(): void {
-    this.getAllProducts();
-  }
-  getAllProducts(){
-    this.productService.getAllProducts().subscribe((data)=>{
-      this.products=data
-    })
-  }
-
-  navigateToReview(productId:number){
-    this.router.navigate(['/review',productId])
-  }
-
-  navigateToMyReview(){
-    this.router.navigate(['/viewReview'])
-  }
-
-  getImageUrl(imageName: string): string {
-    let a = this.ApiUrl+"/"+imageName
-    console.log(a)
-    a='https://8080-fdefbfaaafbdebbfaadbececcfeddbcfdcfcc.premiumproject.examly.io/1746002370897_Image.jpg'
-    console.log(a)
-    return `${this.ApiUrl}/${imageName}`;
-  }
-
+ 
+    products: Product[] = [];
+    ApiUrl: string = Api.apiUrl;
   
-addToCart(product: Product): void {
-      if (this.quantity && this.quantity > 0) {
-        this.cartService.addToCart(product,this.quantity);
-        alert('Product added to cart!');
-      } else {
-        alert('Please enter a valid quantity.');
-      }
+    constructor(private router: Router, private productService: ProductService, private cartService: CartService) { }
   
+    ngOnInit(): void {
+      this.getAllProducts();
+    }
+  
+   
+getAllProducts() {
+  this.productService.getAllProducts().subscribe((data) => {
+    this.products = data;
+    // Initialize quantity for each product
+    this.products.forEach(product => product.quantity);
+  });
 }
+
+  
+    navigateToReview(productId: number) {
+      this.router.navigate(['/review', productId]);
+    }
+  
+    navigateToMyReview() {
+      this.router.navigate(['/viewReview']);
+    }
+  
+    getImageUrl(imageName: string): string {
+      let a = this.ApiUrl + "/" + imageName;
+      console.log(a);
+      a = 'https://8080-fdefbfaaafbdebbfaadbececcfeddbcfdcfcc.premiumproject.examly.io/1746002370897_Image.jpg';
+      console.log(a);
+      return `${this.ApiUrl}/${imageName}`;
+    }
+  
+    addToCart(product: Product): void {
+    if (product.quantity && product.quantity > 0) {
+      if (product.quantity <= product.stockQuantity) {
+        this.cartService.addToCart(product, product.quantity);
+        alert('Product added to cart!');
+      } else {
+        alert('Quantity exceeds available stock.');
+      }
+    } else {
+      alert('Please enter a valid quantity.');
+    }
+  }
+  
 }
